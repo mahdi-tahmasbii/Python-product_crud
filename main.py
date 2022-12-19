43bc41a0a83c3f9f106c6631796787c5e6a9eafc
mahdi-tahmasbii <mahdi_tahmasbi11@yahoo.com>
2022-12-19 16:42:24 +0330

    add product

--- main.py
+++ main.py
@@ -1,39 +1,6 @@
-class Products:
-    _products = []
+from product import Products
 
-    def __init__(self, id, title, short_description, description, slug, permalink, is_available, sku, price,
-                 regular_price,
-                 manage_stock, stock_quantity, is_visible, date_created_gmt, date_modified_gmt):
-        self.id = id
-        self.title = title
-        self.short_description = short_description
-        self.description = description
-        self.slug = slug
-        self.permalink = permalink
-        self.is_available = is_available
-        self.sku = sku
-        self.price = price
-        self.regular_price = regular_price
-        self.manage_stock = manage_stock
-        self.stock_quantity = stock_quantity
-        self.is_visible = is_visible
-        self.date_created_gmt = date_created_gmt
-        self.date_modified_gmt = date_modified_gmt
+p1 = Products('1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1')
 
-    def create_product(self, products):
-        return self._products.append(products)
-
-    def read_product(self):
-        products = [print(product) for product in self._products]
-        return products
-
-    def update_product(self, old_product, new_product):
-        for product in self._products:
-            if product == old_product:
-                old_product = new_product
-                print('1')
-            else:
-                print('Product is not in the list')
-
-    def delete_product(self, product):
-        print('List is empty') if self._products == [] else self._products.remove(product)
+p1.create_product(p1)
+print(Products.read_product(p1))
