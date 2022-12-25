99825f21a0a497363aa3a225016fc45073f2bfd1
mahdi-tahmasbii <mahdi_tahmasbi11@yahoo.com>
2022-12-25 16:45:10 +0330

    check  p1 is_instance

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from product import Products
 
 p1 = Products('1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1')
 
+is_instance = isinstance(p1, Products)
+
+print(is_instance)
