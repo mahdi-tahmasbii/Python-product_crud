4380074ad01b71fc96e6321908f37edf4be18d09
mahdi-tahmasbii <mahdi_tahmasbi11@yahoo.com>
2022-12-28 02:49:51 +0330

    I debugged the code and fixed it

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from product import Products
 
-p1 = Products('1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1')
 
-is_instance = isinstance(p1, Products)
+def main():
+    p1 = Products(1, 'lenovo idea pad 3', 'gaming laptop', 'gaming model', 'lenovo-idea-pad-3', 'asda', True, '42', 420,
+                  56,
+                  75, 5, 5, True,
+                  '2021/2/4',
+                  '2021/2/4')
+    p2 = Products(2, 'lenovo think pad', 'gaming laptop', 'gaming model', 'lenovo-idea-pad-3', 'asda', True, '42', 420,
+                  56,
+                  75, 5, 5, True,
+                  '2021/2/4',
+                  '2021/2/4')
 
-print(is_instance)
-print(type(p1))
-print(type(Products))
+    p1.create_product()
+    p1.read_product()
+    p1.update_product(p1, p2)
+    p1.delete_product(p1)
+    print(f'Product ({p1.title}) is Deleted')
+    p1.read_product()
+
+
+if __name__ == "__main__":
+    main()
--- product.py
+++ product.py
@@ -1,9 +1,9 @@
 class Products:
     _products = []
 
-    def __init__(self, id, title, short_description, description, slug, permalink, is_available, sku, price,
-                 regular_price,
-                 manage_stock, stock_quantity, is_visible, date_created_gmt, date_modified_gmt):
+    def __init__(self, id: int, title: str, short_description: str, description: str, slug: str, permalink: str,
+                 is_available: bool, sku: str, price: float, regular_price: float, sale_price: float, manage_stock: int,
+                 stock_quantity: int, is_visible: bool, date_created_gmt, date_modified_gmt):
         self.id = id
         self.title = title
         self.short_description = short_description
@@ -14,11 +14,17 @@ class Products:
         self.sku = sku
         self.price = price
         self.regular_price = regular_price
+        self.sale_price = sale_price
         self.manage_stock = manage_stock
         self.stock_quantity = stock_quantity
         self.is_visible = is_visible
         self.date_created_gmt = date_created_gmt
         self.date_modified_gmt = date_modified_gmt
+
+    def __repr__(self):
+        return f'Product({self.id},{self.title},{self.short_description},{self.description},{self.slug}' \
+               f'{self.sku},{self.permalink},{self.is_available},{self.price},{self.regular_price},{self.sale_price}' \
+               f'{self.manage_stock},{self.stock_quantity},{self.is_visible},{self.date_created_gmt},{self.date_modified_gmt}) '
 
     def create_product(self):
         self._products.append(self)
@@ -31,10 +37,9 @@ class Products:
         for product in self._products:
             if product == old_product:
                 old_product = new_product
-                print('1')
+                print(f'Product updated: {old_product}')
             else:
                 print('Product is not in the list')
 
     def delete_product(self, product):
         print('List is empty') if self._products == [] else self._products.remove(product)
-
